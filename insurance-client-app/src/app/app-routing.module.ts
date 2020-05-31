98bb58596c2dc5389100d2f2256a27f9cdf651c5
Daniel Carlson <dancarl758@gmail.com>
2020-05-31 19:39:39 +0100

    feat(client): Generated modules and created first components using cli

--- insurance-client-app/src/app/app-routing.module.ts
+++ insurance-client-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homepage',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'authentication',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
